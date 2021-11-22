98a96ab29ec9112b7dd2209c2627ee468f54eeb0
박준현(Junhyun Park)/기프티콘개발팀/11ST <junhyune@sk.com>
2021-11-22 18:04:56 +0900

    first

--- Lesson_01/lesson_01.py
+++ Lesson_01/lesson_01.py
@@ -1,1 +1,30 @@
+#문자열
 print ('hellow worldzzz')
+print (print)
+print (1+2)
+print (15 // 3) #몫
+print (15 // 4) #나머지
+print (15 / 4) 
+print (2 ** 3) #2의4승
+
+str_data = "hello"
+int_data = 1
+float_data = 1.1
+bool_data = True
+
+print(str_data, int_data, float_data, bool_data)
+
+print("zzzz", "1111")
+
+# a = int(input("input A = "))
+# b = int(input("input B = "))
+
+# print(a + b)
+
+#type casting
+#float(3) int to float
+#int(3.6) float to int
+#str(1) int to string
+#int("12") string to int
+#type() 함수로 타입을 알수있음
+print (type(int(3.6)))
