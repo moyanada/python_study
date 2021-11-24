663102276ea69ff41bac54a6c918e09cbe8eeecb
JunHyun <moyanada@gmail.com>
2021-11-24 11:38:01 +0900

    lesson

--- Lesson_01/lesson_01.py
+++ Lesson_01/lesson_01.py
@@ -1,12 +1,15 @@
-#문자열
+#문자열 출력
 print ('hellow worldzzz')
+#해당 키워드 함수 여부 확인
 print (print)
+#산식 확인
 print (1+2)
 print (15 // 3) #몫
 print (15 // 4) #나머지
 print (15 / 4) 
 print (2 ** 3) #2의4승
 
+#타입확인
 str_data = "hello"
 int_data = 1
 float_data = 1.1
@@ -14,8 +17,10 @@ bool_data = True
 
 print(str_data, int_data, float_data, bool_data)
 
+#문자열 , 한칸띄고 넣어짐
 print("zzzz", "1111")
 
+# 키입력 방식
 # a = int(input("input A = "))
 # b = int(input("input B = "))
 
